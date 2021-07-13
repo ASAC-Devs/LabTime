b64e3e8970adb6499362dd62c55b8ccc1437c770
NizarAlsaeed <nizar.ayman@gmail.com>
2021-07-14 02:11:14 +0300

    working video , mic, and now will start with screensharing

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,136 +1,213 @@
-import { useState, useEffect, useRef } from "react";
-import Room from './Room'
+import UserVideoComponent from "./UserVideoComponent";
 import { getToken } from "../api/roomsManager";
 import { OpenVidu } from "openvidu-browser";
+import React, { Component } from 'react';
 
-export default function RoomsList() {
+class RoomsList extends Component {
+    constructor(props) {
+        super(props);
 
-    // init States
-    const [mySessionId, setMySessionId] = useState('MainSession');
-    const [myUserName, setMyUserName] = useState('User' + Math.floor(Math.random() * 100));
-    const [session, setSession] = useState(undefined);
-    const [mainStreamManager, setMainStreamManager] = useState(undefined); // Main video of the page, will be 'publisher' or one of the 'subscribers',
-    const [publisher, setPublisher] = useState(undefined);
-    const [subscribers, setSubscribers] = useState([]);
-    const OV = useRef();
-    const isInitialMount = useRef(true);
-    /*----------------------------------------------------*/
-    function selectThisRoom(e) {
-        e.preventDefault();
-        setMySessionId(e.target.room.value);
-        joinSession();
+        this.state = {
+            mySessionId: 'SessionA',
+            myUserName: 'Participant' + Math.floor(Math.random() * 100),
+            session: undefined,
+            mainStreamManager: undefined,
+            publisher: undefined,
+            subscribers: [],
+            mic:true,
+            camera:true
+        };
+
+        this.joinSession = this.joinSession.bind(this);
+        this.leaveSession = this.leaveSession.bind(this);
+        this.handleChangeSessionId = this.handleChangeSessionId.bind(this);
+        this.handleChangeUserName = this.handleChangeUserName.bind(this);
+        this.handleMainVideoStream = this.handleMainVideoStream.bind(this);
+        this.onbeforeunload = this.onbeforeunload.bind(this);
+        this.handleToggleAudio = this.handleToggleAudio.bind(this);
+        this.handleToggleVideo = this.handleToggleVideo.bind(this);
     }
-    // whenever the sessionId changes (when choosing a room) then call the joinSession func.
-    /*----------------------------------------------------*/
-    function joinSession() {
-        // create session
-        OV.current = new OpenVidu()
-        setSession(OV.current.initSession());
-        // after the session is created, do..
+
+    componentDidMount() {
+        window.addEventListener('beforeunload', this.onbeforeunload);
     }
-    useEffect(() => {
-        if (isInitialMount.current) {
-            isInitialMount.current = false;
-        } else if (session) {
-            const currentSession = session;
 
-            currentSession.on('streamCreated', (event) => {
-                debugger;
-                console.log('streamCreated');
-                const newSubscriber = currentSession.subscribe(event.stream, undefined);
-                console.log('before push',subscribers);
-                const theSubscribers = subscribers;
-                theSubscribers.push(newSubscriber);
-                setSubscribers(theSubscribers);
-            });
+    componentWillUnmount() {
+        window.removeEventListener('beforeunload', this.onbeforeunload);
+    }
 
-            console.log('After push',subscribers);
+    onbeforeunload(event) {
+        this.leaveSession();
+    }
 
-            currentSession.on('streamDestroyed', (event) => {
-                console.warn('streamDestroyed')
-                deleteSubscriber(event.stream.streamManager);
-            });
-            currentSession.on('exception', (exception) => {
-                console.warn(exception);
-            });
+    handleChangeSessionId(e) {
+        this.setState({
+            mySessionId: e.target.value,
+        });
+    }
 
-            getToken().then(token => {
-                currentSession.connect(token, { clientData: myUserName })
-                    .then(() => {
-                        let publisher = OV.current.initPublisher(undefined, {
-                            audioSource: undefined,
-                            videoSource: undefined,
-                            publishAudio: false,
-                            publishVideo: true,
-                            resolution: '640x480',
-                            frameRate: 30,
-                            insertMode: 'APPEND',
-                            mirror: false
-                        });
-                        currentSession.publish(publisher);
-                        setMainStreamManager(publisher);
-                        setPublisher(publisher);
-                    })
-                    .catch(error => {
-                        console.log('There was an error connecting to the session:', error.code, error.message);
-                    });
+    handleChangeUserName(e) {
+        this.setState({
+            myUserName: e.target.value,
+        });
+    }
 
+    handleMainVideoStream(stream) {
+        if (this.state.mainStreamManager !== stream) {
+            this.setState({
+                mainStreamManager: stream
             });
         }
-    }, [session]);
-
-    /*----------------------------------------------------*/
-    function leaveSession() {
-        const currentSession = session;
-        if (currentSession){
-            currentSession.disconnect();
-        }
-        OV.current = null;
-        setPublisher(undefined);
-        setSession(undefined);
-        setSubscribers([]);
-        setMainStreamManager(undefined);
-        setMySessionId('MainSession');
-        setMyUserName('undefined');
     }
-    /*----------------------------------------------------*/
-    function deleteSubscriber(streamManager) {
-        let theSubscribers = subscribers;
-        let index = theSubscribers.indexOf(streamManager, 0);
+    handleToggleAudio(){
+        console.log('Trigrered Audio');
+        this.setState({mic:!this.state.mic},
+            ()=>this.state.publisher.publishAudio(this.state.mic)
+            );
+    }
+    handleToggleVideo(){
+        console.log('Trigrered Video');
+        this.setState({camera:!this.state.camera},
+            ()=>this.state.publisher.publishVideo(this.state.camera)
+            );
+    }
+
+    deleteSubscriber(streamManager) {
+        let subscribers = this.state.subscribers;
+        let index = subscribers.indexOf(streamManager, 0);
         if (index > -1) {
-            theSubscribers.splice(index, 1);
-            setSubscribers(theSubscribers)
+            subscribers.splice(index, 1);
+            this.setState({
+                subscribers: subscribers,
+            });
         }
     }
+
+    joinSession() {
+        this.OV = new OpenVidu();
+        this.setState(
+            {
+                session: this.OV.initSession(),
+            },
+            () => {
+                let mySession = this.state.session;
+                mySession.on('streamCreated', (event) => {
+                    let subscriber = mySession.subscribe(event.stream, undefined);
+                    let subscribers = this.state.subscribers;
+                    subscribers.push(subscriber);
+                    this.setState({
+                        subscribers: subscribers,
+                    });
+                });
+
+                mySession.on('streamDestroyed', (event) => {
+                    this.deleteSubscriber(event.stream.streamManager);
+                });
+
+                mySession.on('exception', (exception) => {
+                    console.warn(exception);
+                });
+
+                getToken(this.state.mySessionId).then((token) => {
+                    mySession.connect(token,{ clientData: this.state.myUserName },
+                        ).then(() => {
+                            let publisher = this.OV.initPublisher(undefined, {
+                                audioSource: undefined, 
+                                videoSource: undefined, 
+                                publishAudio: true, 
+                                publishVideo: true, 
+                                resolution: '640x480', 
+                                frameRate: 30, 
+                                insertMode: 'APPEND',  
+                                mirror: false,
+                            });
+
+                            mySession.publish(publisher);
+
+                            this.setState({
+                                mainStreamManager: publisher,
+                                publisher: publisher,
+                            });
+                        })
+                        .catch((error) => {
+                            console.log('There was an error connecting to the session:', error.code, error.message);
+                        });
+                });
+            },
+        );
+    }
+
+    leaveSession() {
+
+        const mySession = this.state.session;
+
+        if (mySession) {
+            mySession.disconnect();
+        }
+
+        this.OV = null;
+        this.setState({
+            session: undefined,
+            subscribers: [],
+            mySessionId: 'SessionA',
+            myUserName: 'Participant' + Math.floor(Math.random() * 100),
+            mainStreamManager: undefined,
+            publisher: undefined,
+            mic:true,
+            camera:true
+        });
+    }
+
     /*----------------------------------------------------*/
-    if (session) {
-        const aSessionId = mySessionId;
-        const aUserName = myUserName;
-        return (
-            <>
-                <Room
-                    roomName={aSessionId}
-                    userName={aUserName}
-                    publisher={publisher}
-                    subscribers={subscribers} 
-                    leaveSession={leaveSession}/>
-            </>
-        )
+    render() {
+        const mySessionId = this.state.mySessionId;
+        const myUserName = this.state.myUserName;
+        if (this.state.session) {
+            return (
+                <>
+                        <div id="session-header">
+                            <h1 id="session-title">{mySessionId}</h1>
+                            <input
+                                className="btn btn-large btn-danger"
+                                type="button"
+                                id="buttonLeaveSession"
+                                onClick={this.leaveSession}
+                                value="Leave session"
+                            />
+                        <h1>Participants: {this.state.subscribers.length + 1}</h1>
 
-    } else {
-        return (
-            <>
-                <form onSubmit={selectThisRoom}>
-                    <label>room1</label>
-                    <input type="radio" value="room1" name="room" /> <br />
-                    <label>room2</label>
-                    <input type="radio" value="room2" name="room" /> <br />
-                    <button type="submit">confirm</button>
-                </form>
+                        </div>
+                        <button onClick={this.handleToggleAudio}>audio</button>
+                        <button onClick={this.handleToggleVideo}>video</button>
+                        {this.state.publisher !== undefined ? (
+                                <UserVideoComponent
+                                    streamManager={this.state.publisher} />
+                        ) : null}
+                        {this.state.subscribers.map((sub, i) => (
+                                <UserVideoComponent streamManager={sub} />
+                        ))}
+                </>
+            )
 
-                <h1>choose a room to join</h1>
+        } else {
+            return (
+                <>
+                    <form onSubmit={this.joinSession}>
+                        <input type="button" value="room1" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room2" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room3" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room4" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room5" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room6" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room7" onClick={this.handleChangeSessionId} /> <br />
+                        <input type="button" value="room8" onClick={this.handleChangeSessionId} /> <br />
+                        <button type="submit">join</button>
+                    </form>
+                    <h1>choose a room to join</h1>
 
-            </>
-        )
+                </>
+            )
+        }
     }
 }
+export default RoomsList;
