6b4d7289e307acab3f1397634dd7afd5eaaea293
NizarAlsaeed <nizar.ayman@gmail.com>
2021-07-14 15:37:31 +0300

    integrated the conference rooms into the main project but needs styling

--- conference_app/src/conference_page.js
+++ conference_app/src/conference_page.js
@@ -1,16 +1,14 @@
 import TeckitForm from './components/Tickets'
-import AvailableRoom from './components/Roomlist'
+import RoomsList from './components/RoomsList'
 import Header from './components/Header'
 
 export default function Conference() {
     return (
       <div className="w-screen h-screen bg-center bg-no-repeat bg-cover bg-hero-pattern">
-            <Header>
-
-            </Header>
+            <Header />
         <main className="grid grid-cols-5 ml-4 auto-rows-auto">
 
-          <AvailableRoom/>
+          <RoomsList/>
           
         <div className="">
 
