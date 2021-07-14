7e387ac9c724e34d81bcabb6a3a9b6582dbdea2b
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-07-14 10:28:34 +0300

    new app

---
+++ conference_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginForm from './components/LoginForm'; 
+function App() {
+  return (
+    <div className="text-center App">
+      
+        <LoginForm/>
+
+    </div>
+  );
+}
+
+export default App;
